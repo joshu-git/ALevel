438b32ffaa0452ec5d00dabdd976386ce2ab1795
joshu-git <argentjackjoshua@outlook.com>
2025-12-02 06:58:29 +0000

    Setup typescript file structure
    Allows code to be ported to typescript

---
+++ backend-ts/src/menu.ts
@@ -0,0 +1,16 @@
+enum MainMenu {
+    _MainMenu,
+    _Programming,
+    _DataStructures,
+    _Algorithms,
+    _Computation,
+    _DataRepresentation,
+    _ComputerSystems,
+    _OrganizationAndArchitecture,
+    _ConsequencesOfUse,
+    _CommunicationAndNetworking,
+    _Databases,
+    _BigData,
+    _FunctionalProgramming,
+    _ProblemSolving
+}
