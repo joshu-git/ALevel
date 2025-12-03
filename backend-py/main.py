44c261dbc93a19744f8a1b9e4da28dbddf7d8391
joshu-git <argentjackjoshua@outlook.com>
2025-12-03 22:35:38 +0000

    Add health check for main.py
    Tells render or server that it is healthy

--- backend-py/main.py
+++ backend-py/main.py
@@ -21,3 +21,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+#Health check for backend startup
+@app.get("/")
+def root():
+    return {"message": "Backend is running!"}
