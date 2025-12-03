87b7d531f69ebe80d57abdd658be91a954457531
joshu-git <argentjackjoshua@outlook.com>
2025-12-03 22:07:27 +0000

    Setup typescript index.ts
    Allows requests from the frontend

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,21 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+//Load environment variables
+dotenv.config();
 
+//Create the Express app
+const app = express();
 
+//Setup routes here
 
+//Enable CORS for the frontend URL
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+}));
 
-enum MainMenu {
-    _MainMenu,
-    _Programming,
-    _DataStructures,
-    _Algorithms,
-    _Computation,
-    _DataRepresentation,
-    _ComputerSystems,
-    _OrganizationAndArchitecture,
-    _ConsequencesOfUse,
-    _CommunicationAndNetworking,
-    _Databases,
-    _BigData,
-    _FunctionalProgramming,
-    _ProblemSolving
-}
+//Start server using Render Port or local dev
+const Port = process.env.PORT || 5000;
+
+app.listen(Port);
+console.log("ALevel is running on port: " + Port);
